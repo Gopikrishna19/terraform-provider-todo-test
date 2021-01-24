148e21ce2c99006348c16378031e9cb5f12795b9
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2021-01-23 21:21:46 -0600

    create a client to get todos

---
+++ client/client.go
@@ -0,0 +1,75 @@
+package client
+
+import (
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
+)
+
+type Client struct {
+    host   string
+    port   int
+    client *http.Client
+}
+
+func newClient(host string, port int) *Client {
+    return &Client{
+        host:   host,
+        port:   port,
+        client: &http.Client{},
+    }
+}
+
+func (c *Client) get() (*[]Todo, error) {
+    body, err := c.httpRequest("GET", "todo", bytes.Buffer{})
+
+    if err != nil {
+        return nil, err
+    }
+
+    var todos []Todo
+    err = json.NewDecoder(body).Decode(&todos)
+
+    if err != nil {
+        return nil, err
+    }
+
+    return &todos, nil
+}
+
+func (c *Client) httpRequest(method string, path string, body bytes.Buffer) (io.ReadCloser, error) {
+    req, err := http.NewRequest(method, c.getAbsolutePath(path), &body)
+
+    if err != nil {
+        return nil, err
+    }
+
+    if method == "PUT" || method == "POST" {
+        req.Header.Add("Content-Type", "application/json")
+    }
+
+    res, err := c.client.Do(req)
+
+    if err != nil {
+        return nil, err
+    }
+
+    if res.StatusCode != http.StatusOK {
+        resBody := new(bytes.Buffer)
+        _, err = resBody.ReadFrom(res.Body)
+
+        if err != nil {
+            return nil, fmt.Errorf("server responded with an error: %v", res.StatusCode)
+        }
+
+        return nil, fmt.Errorf("server responded with an error: %v - %s", res.StatusCode, resBody.String())
+    }
+
+    return res.Body, nil
+}
+
+func (c *Client) getAbsolutePath(path string) string {
+    return fmt.Sprintf("%s:%v/%s", c.host, c.port, path)
+}
---
+++ client/todo.go
@@ -0,0 +1,7 @@
+package client
+
+type Todo struct {
+    id        int
+    task      string
+    completed bool
+}
